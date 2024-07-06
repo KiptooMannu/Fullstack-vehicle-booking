6b1e5052d6a505010cd53193b609d7c83b89acad
KiptooMannu <Kkgg7241@gmail.com>
2024-07-06 18:05:05 +0300

    added the error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Models, TestimonialsPage, Team, Contact, Home, About } from './pages';
 import { Navbar } from './components';
+import Login from './Features/login/Login';
+import Error from './pages/Error';
 
 import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
-
 import '@fontsource/rubik/300.css';
 import '@fontsource/rubik/400.css';
 import '@fontsource/rubik/500.css';
@@ -18,18 +19,49 @@ import '@fontsource/rubik/700.css';
 import '../src/dist/styles.css';
 
 const App: React.FC = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+      errorElement: <Error />,
+    },
+    {
+      path: 'about',
+      element: <About />,
+      errorElement: <Error />,
+    },
+    {
+      path: 'models',
+      element: <Models />,
+      errorElement: <Error />,
+    },
+    {
+      path: 'testimonials',
+      element: <TestimonialsPage />,
+      errorElement: <Error />,
+    },
+    {
+      path: 'team',
+      element: <Team />,
+      errorElement: <Error />,
+    },
+    {
+      path: 'contact',
+      element: <Contact />,
+      errorElement: <Error />,
+    },
+    {
+      path: 'login',
+      element: <Login />,
+      errorElement: <Error />,
+    }
+  ]);
+
   return (
-    <Router>
+    <>
       <Navbar />
-      <Routes>
-        <Route index path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='models' element={<Models />} />
-        <Route path='testimonials' element={<TestimonialsPage />} />
-        <Route path='team' element={<Team />} />
-        <Route path='contact' element={<Contact />} />
-      </Routes>
-    </Router>
+      <RouterProvider router={router} />
+    </>
   );
 };
 
