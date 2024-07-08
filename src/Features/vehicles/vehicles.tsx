b17496be4e98520cdb472f3561d6336264d3d099
KiptooMannu <Kkgg7241@gmail.com>
2024-07-08 17:01:14 +0300

    BOOKING

--- src/Features/vehicles/vehicles.tsx
+++ src/Features/vehicles/vehicles.tsx
@@ -1,72 +1,42 @@
-import React from 'react';
-import { useGetVehiclesQuery, useCreateVehicleMutation, useUpdateVehicleMutation, useDeleteVehicleMutation } from './vehicleAPI';
 
-interface TVehicle {
-    id: number;
-    category: string;
-    type: string;
-    rentalRate: string;
-    specifications: {
-        imgUrl: string;
-        carName: string;
-        groupSize: number;
-    };
-}
+import "./bookings.scss";
 
-const Vehicles: React.FC = () => {
-  const { data: vehicles, error, isLoading } = useGetVehiclesQuery();
-  const [updateVehicle] = useUpdateVehicleMutation();
-  const [deleteVehicle] = useDeleteVehicleMutation();
-  const [addVehicle] = useCreateVehicleMutation();
+// import carData from "../../assets/dummy-data/booking-cars.js";
+import carItem from "../../AdminDarshboard/components/cars/CarItem";
+import carData from "../assets/dummy-data/booking-cars"
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error loading vehicles</div>;
-  }
-
+const Bookings = () => {
   return (
-    <div className="car__list">
-      {vehicles?.map((vehicle: TVehicle) => {
-        const { category, type, rentalRate, specifications } = vehicle;
-        const { imgUrl, carName, groupSize } = specifications;
-        
-        return (
-          <div key={vehicle.id} className="car__item">
-            <div className="car__item-top">
-              <div className="car__item-tile">
-                <h3>{carName}</h3>
-                <span>
-                  <i className="ri-heart-line"></i>
-                </span>
-              </div>
-              <p>{category}</p>
-            </div>
+    <div className="bookings">
+      <div className="booking__wrapper">
+        <h2 className="booking__title">Booking</h2>
 
-            <div className="car__img">
-              <img src={imgUrl} alt={carName} />
-            </div>
-
-            <div className="car__item-bottom">
-              <div className="car__bottom-left">
-                <p>
-                  <i className="ri-user-line"></i> {groupSize}
-                </p>
-                <p>
-                  <i className="ri-repeat-line"></i>
-                  {type}
-                </p>
-              </div>
-
-              <p className="car__rent">${rentalRate}/d</p>
-            </div>
+        <div className="filter__widget-wrapper">
+          <div className="filter__widget-01">
+            <select>
+              <option value="New">New</option>
+              <option value="Popular">Popular</option>
+              <option value="Upcoming">Upcoming</option>
+            </select>
           </div>
-        );
-      })}
+
+          <div className="filter__widget-01">
+            <select>
+              <option value="toyota">Toyota</option>
+              <option value="bmw">Bmw</option>
+              <option value="audi">Audi</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="booking__car-list">
+          {carData?.map((item) => (
+            <CarItem item={item} key={item.id} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Vehicles;
+export default Bookings;
