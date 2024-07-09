9bea6a63d3ac6abdb5a1075c10fa3ec90e7e2c02
KiptooMannu <Kkgg7241@gmail.com>
2024-07-09 10:29:57 +0300

    registration page

--- src/AdminDarshboard/Darshboard.tsx
+++ src/AdminDarshboard/Darshboard.tsx
@@ -2,13 +2,15 @@
 import './assets/libs/boxicons-2.1.1/css/boxicons.min.css'
 import './scss/App.scss'
 import {  Routes, Route } from 'react-router-dom'
-import Blank from './pages/Blank'
+// import Blank from './pages/Blank'
 import Dashboard from './pages/Darshboard'
 import MainLayout from './layout/MainLayout'
 import User from './pages/Users'
 import Vehicles from './pages/Vehicles'
 import Bookings from './pages/Bookings'
 import Transactions from './pages/Transactions'
+import FleetManagementTable from '../Features/Fleet/FleetTable'
+import Profile from './pages/Profile'
 
 function Darshboard() {
     return (
@@ -19,7 +21,8 @@ function Darshboard() {
                     <Route path="bookings" element={<Bookings />} />
                     <Route path="vehicles" element={<Vehicles />} />
                     <Route path="customers" element={<User />} />
-                    <Route path="settings" element={<Blank />} />
+                    <Route path="profile" element={<Profile />} />
+                    <Route path='fleet-management' element={<FleetManagementTable />}/>
                     <Route path="transactions" element={<Transactions />} />
                 </Route>
             </Routes>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,41 +4,66 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import styles from '../styles/forms/Register.module.scss';
 import { Navbar } from '../components';
+import { Toaster, toast } from 'sonner';
+import { useCreateUserMutation } from '../Features/users/UsersAPI';
 
 type FormValues = {
-  username: string;
+  fullName: string;
   email: string;
+  contactPhone: string;
+  address: string;
   password: string;
 };
 
 const Register: React.FC = () => {
   const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
-  const onSubmit = (data: FormValues) => console.log(data);
+  const [createUser] = useCreateUserMutation();
+
+  const onSubmit = async (data: FormValues) => {
+    try {
+      console.log(data)
+      await createUser(data).unwrap();
+      toast.success('User registered successfully');
+    } catch (error) {
+      toast.error('Error registering user');
+    }
+  };
 
   return (
     <>
-    <Navbar />
-    <div className={styles['register-container']}>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles['register-form']}>
-        <h2>Register</h2>
-        <div className={styles['form-group']}>
-          <label>Username</label>
-          <input {...register('username', { required: true })} type="text" />
-          {errors.username && <span className={styles['error']}>Username is required</span>}
-        </div>
-        <div className={styles['form-group']}>
-          <label>Email</label>
-          <input {...register('email', { required: true })} type="email" />
-          {errors.email && <span className={styles['error']}>Email is required</span>}
-        </div>
-        <div className={styles['form-group']}>
-          <label>Password</label>
-          <input {...register('password', { required: true })} type="password" />
-          {errors.password && <span className={styles['error']}>Password is required</span>}
-        </div>
-        <button type='submit' className={styles['submit-btn']}>Register</button>
-      </form>
-    </div>
+      <Navbar />
+      <Toaster />
+      <div className={styles['register-container']}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles['register-form']}>
+          <h2>Register</h2>
+          <div className={styles['form-group']}>
+            <label>Full Name</label>
+            <input {...register('fullName', { required: true })} type="text" />
+            {errors.fullName && <span className={styles['error']}>Full Name is required</span>}
+          </div>
+          <div className={styles['form-group']}>
+            <label>Email</label>
+            <input {...register('email', { required: true })} type="email" />
+            {errors.email && <span className={styles['error']}>Email is required</span>}
+          </div>
+          <div className={styles['form-group']}>
+            <label>Contact Phone</label>
+            <input {...register('contactPhone', { required: true })} type="text" />
+            {errors.contactPhone && <span className={styles['error']}>Contact Phone is required</span>}
+          </div>
+          <div className={styles['form-group']}>
+            <label>Address</label>
+            <input {...register('address', { required: true })} type="text" />
+            {errors.address && <span className={styles['error']}>Address is required</span>}
+          </div>
+          <div className={styles['form-group']}>
+            <label>Password</label>
+            <input {...register('password', { required: true, minLength: 6 })} type="password" />
+            {errors.password && <span className={styles['error']}>Password must be at least 6 characters</span>}
+          </div>
+          <button type='submit' className={styles['submit-btn']}>Register</button>
+        </form>
+      </div>
     </>
   );
 };
