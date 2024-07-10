0fc6a8c1307bb47e4ac725a8425938b7287209db
KiptooMannu <Kkgg7241@gmail.com>
2024-07-10 20:17:42 +0300

    suport tickets

---
+++ src/Features/SupportTickets/SupportAPI.ts
@@ -0,0 +1,68 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Define the interface for a support ticket
+export interface TSupportTicket {
+  ticketId: number;
+  userId: number;
+  subject: string;
+  description: string;
+  status: string;
+  priority: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Define the API slice for support tickets
+export const supportTicketAPI = createApi({
+  reducerPath: 'supportTicketAPI',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://car-rental-backend-1.onrender.com/api   ' }),
+  tagTypes: ['SupportTickets'],
+  endpoints: (builder) => ({
+    getSupportTickets: builder.query<TSupportTicket[], void>({
+      query: () => 'support-tickets',
+      providesTags: (result) =>
+        result
+          ? [...result.map(({ ticketId }) => ({ type: 'SupportTickets', ticketId } as const)), { type: 'SupportTickets', id: 'LIST' }]
+          : [{ type: 'SupportTickets', id: 'LIST' }],
+    }),
+    getSupportTicketsByUserId: builder.query<TSupportTicket[], number>({
+      query: (userId) => `support-tickets/user/${userId}`,
+      providesTags: (result, error, userId) =>
+        result
+          ? [...result.map(({ ticketId }) => ({ type: 'SupportTickets', ticketId } as const)), { type: 'SupportTickets', userId }]
+          : [{ type: 'SupportTickets', userId }],
+    }),
+    createSupportTicket: builder.mutation<TSupportTicket, Partial<TSupportTicket>>({
+      query: (newTicket) => ({
+        url: 'support-tickets',
+        method: 'POST',
+        body: newTicket,
+      }),
+      invalidatesTags: [{ type: 'SupportTickets', id: 'LIST' }],
+    }),
+    updateSupportTicket: builder.mutation<TSupportTicket, Partial<TSupportTicket>>({
+      query: ({ ticketId, ...rest }) => ({
+        url: `support-tickets/${ticketId}`,
+        method: 'PUT',
+        body: rest,
+      }),
+      invalidatesTags: (result, error, { ticketId }) => [{ type: 'SupportTickets', ticketId }],
+    }),
+    deleteSupportTicket: builder.mutation<{ success: boolean; id: number }, number>({
+      query: (id) => ({
+        url: `support-tickets/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, id) => [{ type: 'SupportTickets', id }],
+    }),
+  }),
+});
+
+// Export the auto-generated hooks
+export const {
+  useGetSupportTicketsQuery,
+  useGetSupportTicketsByUserIdQuery,
+  useCreateSupportTicketMutation,
+  useUpdateSupportTicketMutation,
+  useDeleteSupportTicketMutation
+} : any = supportTicketAPI;
--- src/pages/Models.tsx
+++ src/pages/Models.tsx
@@ -9,64 +9,64 @@ import { CAR_DATA } from '../components/CarData';
 const Models: React.FC = () => {
   return (
     <>
-    <Navbar />
-    <section className='models-section'>
-      <HeroPages name='Modelos de Carros' />
-      <div className='container'>
-        <div className='models-div'>
-          {/* models */}
-          {CAR_DATA.map((car, index) => (
-            <div key={index} className='models-div__box'>
-              <div className='models-div__box__img'>
-                <img src={car[0].img} alt='' width='270' height='180' />
-                <div className='models-div__box__descr'>
-                  <div className='models-div__box__descr__name-price'>
-                    <div className='models-div__box__descr__name-price__name'>
-                      <p>{car[0].model}</p>
+      <Navbar />
+      <section className='models-section'>
+        <HeroPages name='Car Models' />
+        <div className='container'>
+          <div className='models-div'>
+            {/* models */}
+            {CAR_DATA.map((car, index) => (
+              <div key={index} className='models-div__box'>
+                <div className='models-div__box__img'>
+                  <img src={car[0].img} alt='' width='270' height='180' />
+                  <div className='models-div__box__descr'>
+                    <div className='models-div__box__descr__name-price'>
+                      <div className='models-div__box__descr__name-price__name'>
+                        <p>{car[0].model}</p>
+                        <span>
+                          <IconStar width={15} height={15} />
+                          <IconStar width={15} height={15} />
+                          <IconStar width={15} height={15} />
+                          <IconStar width={15} height={15} />
+                          <IconStar width={15} height={15} />
+                        </span>
+                      </div>
+                      <div className='models-div__box__descr__name-price__price'>
+                        <h4>${car[0].price}</h4>
+                        <p>per day</p>
+                      </div>
+                    </div>
+                    <div className='models-div__box__descr__name-price__details'>
                       <span>
-                        <IconStar width={15} height={15} />
-                        <IconStar width={15} height={15} />
-                        <IconStar width={15} height={15} />
-                        <IconStar width={15} height={15} />
-                        <IconStar width={15} height={15} />
+                        <IconCar /> &nbsp; {car[0].mark}
+                      </span>
+                      <span style={{ textAlign: 'right' }}>
+                        {car[0].doors} &nbsp; <IconCar />
+                      </span>
+                      <span>
+                        <IconCar /> &nbsp;{' '}
+                        {car[0].transmission === 'Automático' ? 'Automatic' : car[0].transmission}
+                      </span>
+                      <span style={{ textAlign: 'right' }}>
+                        {car[0].fuel} &nbsp; <IconCar />
                       </span>
                     </div>
-                    <div className='models-div__box__descr__name-price__price'>
-                      <h4>R${car[0].price}</h4>
-                      <p>por dia</p>
+                    <div className='models-div__box__descr__name-price__btn'>
+                      <Link onClick={() => window.scrollTo(0, 0)} to='/login'>
+                        Book Now
+                      </Link>
                     </div>
-                  </div>
-                  <div className='models-div__box__descr__name-price__details'>
-                    <span>
-                      <IconCar /> &nbsp; {car[0].mark}
-                    </span>
-                    <span style={{ textAlign: 'right' }}>
-                      {car[0].doors} &nbsp; <IconCar />
-                    </span>
-                    <span>
-                      <IconCar /> &nbsp;{' '}
-                      {car[0].transmission === 'Automático' ? 'Auto' : car[0].transmission}
-                    </span>
-                    <span style={{ textAlign: 'right' }}>
-                      {car[0].fuel} &nbsp; <IconCar />
-                    </span>
-                  </div>
-                  <div className='models-div__box__descr__name-price__btn'>
-                    <Link onClick={() => window.scrollTo(0, 0)} to='/'>
-                      Quero Esse
-                    </Link>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-      {/* book banner */}
-      <BookBanner />
-      {/* footer */}
-      <Footer />
-    </section>
+        {/* book banner */}
+        <BookBanner />
+        {/* footer */}
+        <Footer />
+      </section>
     </>
   );
 };
