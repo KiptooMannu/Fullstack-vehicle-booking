7d781f572a8f1f66158a4c9b66a8076a605a0f32
KiptooMannu <Kkgg7241@gmail.com>
2024-07-14 16:49:20 +0300

    mui login page

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,181 +1,181 @@
-import React, { useState } from 'react';
-// import { RouteComponentProps } from 'react-router-dom';
-import './login.scss';
-import logo from '../../assets/images/logo.png';
-import facebook from '../../assets/images/facebook.svg';
-import google from '../../assets/images/google.svg';
-import linkedin from '../../assets/images/linkedin.svg';
-import { connect } from 'react-redux';
-// import { setUserAction } from '../../shared/store/actions/user.actions';
-// import { Dispatch } from 'redux';
+// import React, { useState } from 'react';
+// // import { RouteComponentProps } from 'react-router-dom';
+// import './login.scss';
+// import logo from '../../assets/images/logo.png';
+// import facebook from '../../assets/images/facebook.svg';
+// import google from '../../assets/images/google.svg';
+// import linkedin from '../../assets/images/linkedin.svg';
+// import { connect } from 'react-redux';
+// // import { setUserAction } from '../../shared/store/actions/user.actions';
+// // import { Dispatch } from 'redux';
 
-// interface Props extends RouteComponentProps {
-//   setUserType(username: string): void;
-// }
+// // interface Props extends RouteComponentProps {
+// //   setUserType(username: string): void;
+// // }
 
-const Login = ({ setUserType, history }: any) => {
-  const [login, setLogin] = useState(true);
-  const [signUpForm, setSignUpForm] = useState({
-    name: '',
-    email: '',
-    password: '',
-  });
-  const [signInForm, setSignInForm] = useState({
-    email: '',
-    password: '',
-  });
+// const Login = ({ setUserType, history }: any) => {
+//   const [login, setLogin] = useState(true);
+//   const [signUpForm, setSignUpForm] = useState({
+//     name: '',
+//     email: '',
+//     password: '',
+//   });
+//   const [signInForm, setSignInForm] = useState({
+//     email: '',
+//     password: '',
+//   });
 
-  const handleSignUpChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setSignUpForm((prevForm) => ({ ...prevForm, [name]: value }));
-  };
-
-  const handleSignInChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setSignInForm((prevForm) => ({ ...prevForm, [name]: value }));
-  };
-
-  const signUp = () => {
-    setSignUpForm({
-      name: '',
-      password: '',
-      email: '',
-    });
-  };
-
-  const signIn = () => {
-    history.push('/dashboard');
-    setUserType(signInForm.email);
-    setSignInForm({
-      password: '',
-      email: '',
-    });
-  };
-
-  return (
-    <div className="login">
-      <div className={`login__colored-container ${login ? 'login__colored-container--left' : 'login__colored-container--right'}`}></div>
-      <div className={`login__welcome-back ${login ? 'login__welcome-back--active' : 'login__welcome-back--inactive'}`}>
-        <div className="login__welcome-back__logo-container">
-          <img className="login__welcome-back__logo-container--image" src={logo} alt="Budwriter" />
-          Budwriter
-        </div>
-        <div className="login__welcome-back__main-container">
-          <div className="login__welcome-back__main-container__text-container">
-            <span className="login__welcome-back__main-container__text-container--title">Welcome Back!</span>
-            <span className="login__welcome-back__main-container__text-container--secondary">To keep sharing your work with us, please log in.</span>
-          </div>
-          <div onClick={() => setLogin(!login)} className="login__welcome-back__main-container__button-container">
-            Sign In
-          </div>
-        </div>
-      </div>
-      <div className={`login__create-container ${login ? 'login__create-container--active' : 'login__create-container--inactive'}`}>
-        Create Account
-        <div className="login__create-container__social-container">
-          <img className="login__create-container__social-container--facebook-icon" src={facebook} alt="" />
-          <img className="login__create-container__social-container--google-icon" src={google} alt="" />
-          <img className="login__create-container__social-container--linkedin-icon" src={linkedin} alt="" />
-        </div>
-        <span className="login__create-container--info-text">or use email for your registration</span>
-        <div className="login__create-container__form-container">
-          <form className="login__create-container__form-container__form" onSubmit={(e) => {
-            e.preventDefault();
-            signUp();
-          }}>
-            <input
-              className="login__create-container__form-container__form--name"
-              type="text"
-              placeholder="Name"
-              name="name"
-              value={signUpForm.name}
-              onChange={handleSignUpChange}
-              required
-            />
-            <input
-              className="login__create-container__form-container__form--email"
-              type="email"
-              placeholder="Email"
-              name="email"
-              value={signUpForm.email}
-              onChange={handleSignUpChange}
-              required
-            />
-            <input
-              className="login__create-container__form-container__form--password"
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={signUpForm.password}
-              onChange={handleSignUpChange}
-              required
-            />
-            <button className="login__create-container__form-container__form--submit">Sign Up</button>
-          </form>
-        </div>
-      </div>
-      <div className={`login__login-container ${!login ? 'login__login-container--active' : 'login__login-container--inactive'}`}>
-        <div className="login__login-container__logo-container">
-          <img className="login__login-container__logo-container--image" src={logo} alt="Budwriter" />
-          Budwriter
-        </div>
-        <div className="login__login-container__main-container">
-          <div className="login__login-container__main-container__social-container">
-            <img className="login__login-container__main-container__social-container--facebook-icon" src={facebook} alt="" />
-            <img className="login__login-container__main-container__social-container--google-icon" src={google} alt="" />
-            <img className="login__login-container__main-container__social-container--linkedin-icon" src={linkedin} alt="" />
-          </div>
-          <span className="login__login-container__main-container--info-text">or use email for your login</span>
-          <div className="login__login-container__main-container__form-container">
-            <form className="login__login-container__main-container__form-container__form" onSubmit={(e) => {
-              e.preventDefault();
-              signIn();
-            }}>
-              <input
-                className="login__login-container__main-container__form-container__form--email"
-                type="email"
-                placeholder="Email"
-                name="email"
-                value={signInForm.email}
-                onChange={handleSignInChange}
-                required
-              />
-              <input
-                className="login__login-container__main-container__form-container__form--password"
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={signInForm.password}
-                onChange={handleSignInChange}
-                required
-              />
-              <button className="login__login-container__main-container__form-container__form--submit">Sign In</button>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div className={`login__hello-container ${!login ? 'login__hello-container--active' : 'login__hello-container--inactive'}`}>
-        <div className="login__welcome-back__main-container__text-container">
-          <span className="login__welcome-back__main-container__text-container--title">Hello, stranger!</span>
-          <span className="login__welcome-back__main-container__text-container--secondary">Enter your personal details and start your own portfolio!</span>
-        </div>
-        <div onClick={() => setLogin(!login)} className="login__welcome-back__main-container__button-container">
-          Sign Up
-        </div>
-      </div>
-    </div>
-  );
-};
-
-// Uncomment and update the below code as per your Redux setup
-// function mapDispatchToProps(dispatch: Dispatch) {
-//   return {
-//     setUserType: (username: string) =>
-//       dispatch(setUserAction({
-//           username
-//       }))
+//   const handleSignUpChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setSignUpForm((prevForm) => ({ ...prevForm, [name]: value }));
 //   };
-// }
 
-// export default connect(null, mapDispatchToProps)(Login);
-export default Login;
+//   const handleSignInChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setSignInForm((prevForm) => ({ ...prevForm, [name]: value }));
+//   };
+
+//   const signUp = () => {
+//     setSignUpForm({
+//       name: '',
+//       password: '',
+//       email: '',
+//     });
+//   };
+
+//   const signIn = () => {
+//     history.push('/dashboard');
+//     setUserType(signInForm.email);
+//     setSignInForm({
+//       password: '',
+//       email: '',
+//     });
+//   };
+
+//   return (
+//     <div className="login">
+//       <div className={`login__colored-container ${login ? 'login__colored-container--left' : 'login__colored-container--right'}`}></div>
+//       <div className={`login__welcome-back ${login ? 'login__welcome-back--active' : 'login__welcome-back--inactive'}`}>
+//         <div className="login__welcome-back__logo-container">
+//           <img className="login__welcome-back__logo-container--image" src={logo} alt="Budwriter" />
+//           Budwriter
+//         </div>
+//         <div className="login__welcome-back__main-container">
+//           <div className="login__welcome-back__main-container__text-container">
+//             <span className="login__welcome-back__main-container__text-container--title">Welcome Back!</span>
+//             <span className="login__welcome-back__main-container__text-container--secondary">To keep sharing your work with us, please log in.</span>
+//           </div>
+//           <div onClick={() => setLogin(!login)} className="login__welcome-back__main-container__button-container">
+//             Sign In
+//           </div>
+//         </div>
+//       </div>
+//       <div className={`login__create-container ${login ? 'login__create-container--active' : 'login__create-container--inactive'}`}>
+//         Create Account
+//         <div className="login__create-container__social-container">
+//           <img className="login__create-container__social-container--facebook-icon" src={facebook} alt="" />
+//           <img className="login__create-container__social-container--google-icon" src={google} alt="" />
+//           <img className="login__create-container__social-container--linkedin-icon" src={linkedin} alt="" />
+//         </div>
+//         <span className="login__create-container--info-text">or use email for your registration</span>
+//         <div className="login__create-container__form-container">
+//           <form className="login__create-container__form-container__form" onSubmit={(e) => {
+//             e.preventDefault();
+//             signUp();
+//           }}>
+//             <input
+//               className="login__create-container__form-container__form--name"
+//               type="text"
+//               placeholder="Name"
+//               name="name"
+//               value={signUpForm.name}
+//               onChange={handleSignUpChange}
+//               required
+//             />
+//             <input
+//               className="login__create-container__form-container__form--email"
+//               type="email"
+//               placeholder="Email"
+//               name="email"
+//               value={signUpForm.email}
+//               onChange={handleSignUpChange}
+//               required
+//             />
+//             <input
+//               className="login__create-container__form-container__form--password"
+//               type="password"
+//               placeholder="Password"
+//               name="password"
+//               value={signUpForm.password}
+//               onChange={handleSignUpChange}
+//               required
+//             />
+//             <button className="login__create-container__form-container__form--submit">Sign Up</button>
+//           </form>
+//         </div>
+//       </div>
+//       <div className={`login__login-container ${!login ? 'login__login-container--active' : 'login__login-container--inactive'}`}>
+//         <div className="login__login-container__logo-container">
+//           <img className="login__login-container__logo-container--image" src={logo} alt="Budwriter" />
+//           Budwriter
+//         </div>
+//         <div className="login__login-container__main-container">
+//           <div className="login__login-container__main-container__social-container">
+//             <img className="login__login-container__main-container__social-container--facebook-icon" src={facebook} alt="" />
+//             <img className="login__login-container__main-container__social-container--google-icon" src={google} alt="" />
+//             <img className="login__login-container__main-container__social-container--linkedin-icon" src={linkedin} alt="" />
+//           </div>
+//           <span className="login__login-container__main-container--info-text">or use email for your login</span>
+//           <div className="login__login-container__main-container__form-container">
+//             <form className="login__login-container__main-container__form-container__form" onSubmit={(e) => {
+//               e.preventDefault();
+//               signIn();
+//             }}>
+//               <input
+//                 className="login__login-container__main-container__form-container__form--email"
+//                 type="email"
+//                 placeholder="Email"
+//                 name="email"
+//                 value={signInForm.email}
+//                 onChange={handleSignInChange}
+//                 required
+//               />
+//               <input
+//                 className="login__login-container__main-container__form-container__form--password"
+//                 type="password"
+//                 placeholder="Password"
+//                 name="password"
+//                 value={signInForm.password}
+//                 onChange={handleSignInChange}
+//                 required
+//               />
+//               <button className="login__login-container__main-container__form-container__form--submit">Sign In</button>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//       <div className={`login__hello-container ${!login ? 'login__hello-container--active' : 'login__hello-container--inactive'}`}>
+//         <div className="login__welcome-back__main-container__text-container">
+//           <span className="login__welcome-back__main-container__text-container--title">Hello, stranger!</span>
+//           <span className="login__welcome-back__main-container__text-container--secondary">Enter your personal details and start your own portfolio!</span>
+//         </div>
+//         <div onClick={() => setLogin(!login)} className="login__welcome-back__main-container__button-container">
+//           Sign Up
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// // Uncomment and update the below code as per your Redux setup
+// // function mapDispatchToProps(dispatch: Dispatch) {
+// //   return {
+// //     setUserType: (username: string) =>
+// //       dispatch(setUserAction({
+// //           username
+// //       }))
+// //   };
+// // }
+
+// // export default connect(null, mapDispatchToProps)(Login);
+// export default Login;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,67 +1,169 @@
-// src/pages/Login.tsx
+import React, { useState } from 'react';
+// import '../components/login/login.scss';
+// import logo from '../assets/images/logo.png';
+// import facebook from '../assets/images/facebook.svg';
+// import google from '../assets/images/google.svg';
+// import linkedin from '../assets/images/linkedin.svg';
 import { useForm } from "react-hook-form";
 import { useLoginUserMutation } from '../Features/login/loginAPI';
 import { useNavigate } from 'react-router-dom';
-import styles from '../styles/forms/Login.module.scss';
-// import { LoginResponse } from '../Features/login/loginAPI'; // Import the LoginResponse type
-import { Navbar } from "../components";
 
-type FormValues = {
-    email: string;
-    password: string;
+const Login = () => {
+  const [login, setLogin] = useState(true);
+  const [signUpForm, setSignUpForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [loginUser, { isLoading, error }] = useLoginUserMutation();
+  const navigate = useNavigate();
+
+  const handleSignUpChange = (e: any) => {
+    const { name, value } = e.target;
+    setSignUpForm((prevForm) => ({ ...prevForm, [name]: value }));
+  };
+
+  const signUp = () => {
+    setSignUpForm({
+      name: '',
+      password: '',
+      email: '',
+    });
+  };
+
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+      const response = await loginUser(data).unwrap();
+      console.log(response);
+
+      localStorage.setItem('user', JSON.stringify(response));
+      localStorage.setItem('token', JSON.stringify(response.token));
+
+      const userRole = response.user.role;
+      if (userRole === 'admin') {
+        navigate('/admin');
+      } else if (userRole === 'user') {
+        navigate('/users');
+      }
+    } catch (error) {
+      console.error('Failed to login', error);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className={`login__colored-container ${login ? 'login__colored-container--left' : 'login__colored-container--right'}`}></div>
+      <div className={`login__welcome-back ${login ? 'login__welcome-back--active' : 'login__welcome-back--inactive'}`}>
+        <div className="login__welcome-back__logo-container">
+          <img className="login__welcome-back__logo-container--image" src={logo} alt="Budwriter" />
+          Budwriter
+        </div>
+        <div className="login__welcome-back__main-container">
+          <div className="login__welcome-back__main-container__text-container">
+            <span className="login__welcome-back__main-container__text-container--title">Welcome Back!</span>
+            <span className="login__welcome-back__main-container__text-container--secondary">To keep sharing your work with us, please log in.</span>
+          </div>
+          <div onClick={() => setLogin(true)} className="login__welcome-back__main-container__button-container">
+            Sign In
+          </div>
+        </div>
+      </div>
+      <div className={`login__create-container ${login ? 'login__create-container--inactive' : 'login__create-container--active'}`}>
+        <div className="login__create-container__form-header">
+          <h2>Create Account</h2>
+          <div className="login__create-container__social-container">
+            <img className="login__create-container__social-container--facebook-icon" src={facebook} alt="Facebook" />
+            <img className="login__create-container__social-container--google-icon" src={google} alt="Google" />
+            <img className="login__create-container__social-container--linkedin-icon" src={linkedin} alt="LinkedIn" />
+          </div>
+          <span className="login__create-container--info-text">or use email for your registration</span>
+        </div>
+        <div className="login__create-container__form-container">
+          <form className="login__create-container__form-container__form" onSubmit={(e) => {
+            e.preventDefault();
+            signUp();
+          }}>
+            <input
+              className="login__create-container__form-container__form--name"
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={signUpForm.name}
+              onChange={handleSignUpChange}
+              required
+            />
+            <input
+              className="login__create-container__form-container__form--email"
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={signUpForm.email}
+              onChange={handleSignUpChange}
+              required
+            />
+            <input
+              className="login__create-container__form-container__form--password"
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={signUpForm.password}
+              onChange={handleSignUpChange}
+              required
+            />
+            <button className="login__create-container__form-container__form--submit">Sign Up</button>
+          </form>
+        </div>
+      </div>
+      <div className={`login__login-container ${!login ? 'login__login-container--inactive' : 'login__login-container--active'}`}>
+        <div className="login__login-container__logo-container">
+          <img className="login__login-container__logo-container--image" src={logo} alt="Budwriter" />
+          Budwriter
+        </div>
+        <div className="login__login-container__main-container">
+          <div className="login__login-container__main-container__social-container">
+            <img className="login__login-container__main-container__social-container--facebook-icon" src={facebook} alt="Facebook" />
+            <img className="login__login-container__main-container__social-container--google-icon" src={google} alt="Google" />
+            <img className="login__login-container__main-container__social-container--linkedin-icon" src={linkedin} alt="LinkedIn" />
+          </div>
+          <span className="login__login-container__main-container--info-text">or use email for your login</span>
+          <div className="login__login-container__main-container__form-container">
+            <form className="login__login-container__main-container__form-container__form" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className="login__login-container__main-container__form-container__form--email"
+                type="email"
+                placeholder="Email"
+                {...register("email", { required: true })}
+                required
+              />
+              {errors.email && <span className="error">Email is required</span>}
+              <input
+                className="login__login-container__main-container__form-container__form--password"
+                type="password"
+                placeholder="Password"
+                {...register("password", { required: true })}
+                required
+              />
+              {errors.password && <span className="error">Password is required</span>}
+              <button className="login__login-container__main-container__form-container__form--submit" type="submit">Sign In</button>
+              {isLoading && <p className="loading">Loading...</p>}
+              {error && <p className="error-message">Failed to login</p>}
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className={`login__hello-container ${!login ? 'login__hello-container--active' : 'login__hello-container--inactive'}`}>
+        <div className="login__welcome-back__main-container__text-container">
+          <span className="login__welcome-back__main-container__text-container--title">Hello, stranger!</span>
+          <span className="login__welcome-back__main-container__text-container--secondary">Enter your personal details and start your own portfolio!</span>
+        </div>
+        <div onClick={() => setLogin(false)} className="login__welcome-back__main-container__button-container">
+          Sign Up
+        </div>
+      </div>
+    </div>
+  );
 };
-
-function Login() {
-    const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
-    const [loginUser, { isLoading, error }] = useLoginUserMutation();
-    const navigate = useNavigate();
-
-    const onSubmit = async (data: FormValues) => {
-        try {
-            console.log(data);
-            const response = await loginUser(data).unwrap() ;
-            console.log(response);
-
-            // Persist the response to local storage
-            localStorage.setItem('user', JSON.stringify(response));
-            localStorage.setItem('token', JSON.stringify(response.token));
-            
-            console.log(response.token);
-
-            const userRole = response.user.role;
-            if (userRole === 'admin') {
-                navigate('/admin');
-            } else if (userRole === 'user') {
-                navigate('/users');
-            }
-        } catch (error) {
-            console.error('Failed to login', error);
-        }
-    };
-
-    return (
-        <>
-            <Navbar />
-            <div className={styles['login-container']}>
-                <form onSubmit={handleSubmit(onSubmit)} className={styles['login-form']}>
-                    <div className={styles['form-title']}>Login</div>
-                    <div className={styles['form-group']}>
-                        <label>Email</label>
-                        <input {...register("email", { required: true })} type="email" />
-                        {errors.email && <span className={styles['error']}>Email is required</span>}
-                    </div>
-                    <div className={styles['form-group']}>
-                        <label>Password</label>
-                        <input {...register("password", { required: true })} type="password" />
-                        {errors.password && <span className={styles['error']}>Password is required</span>}
-                    </div>
-                    <button type='submit' className={styles['submit-btn']}>Login</button>
-                    {isLoading && <p className={styles['loading']}>Loading...</p>}
-                    {error && <p className={styles['error-message']}>Failed to login</p>}
-                </form>
-            </div>
-        </>
-    );
-}
 
 export default Login;
