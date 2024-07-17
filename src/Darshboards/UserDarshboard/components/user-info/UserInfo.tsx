4afe170cd390dfc227e64b94cc9e95ae48de364a
KiptooMannu <Kkgg7241@gmail.com>
2024-07-17 18:55:14 +0300

    styled the info nav

--- src/Darshboards/UserDarshboard/components/user-info/UserInfo.tsx
+++ src/Darshboards/UserDarshboard/components/user-info/UserInfo.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './user-info.scss';
 import { User } from '../types/user';
+import Upload from '../../../../components/Upload'; // Make sure to import the Upload component
 
 interface UserInfoProps {
-    user: User;
+  user: User;
 }
 
 const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
-    return (
-        <div className='user-info'>
-            <div className="user-info__img">
-                <img src={user.img} alt={user.name} />
-            </div>
-            <div className="user-info__name">
-                <span>{user.name}</span>
-            </div>
+  const [userImage, setUserImage] = useState(user.img);
+
+  const handleOnUpload = (error: any, result: any) => {
+    if (error) {
+      console.error(error.message);
+      return;
+    }
+    setUserImage(result?.info?.secure_url);
+  };
+
+  return (
+    <div className='user-info'>
+      <div className="user-info__img">
+        <img src={userImage} alt={user.name} />
+        <div className="user-info__img-overlay">
+          <Upload onUpload={handleOnUpload} />
+          {/* <button className="user-info__img-button">Add</button> */}
         </div>
-    );
+      </div>
+      <div className="user-info__name">
+        <span>{user.name}</span>
+      </div>
+    </div>
+  );
 }
 
 export default UserInfo;
